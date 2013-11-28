4384a7c5d3386d6dd4bb17d88c9524d0845b819f
Sebastian Real <seba.real.a@gmail.com>
2013-11-28 08:50:22 -0300

    agregando a la lista de inicio de urls la pagina principal

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^$',include("deseos.urls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^templates/.+?/$', 'crowdfunding.views.get_js_template', name="templates"),
     url(r'^registro/',include('registro.urls')),
