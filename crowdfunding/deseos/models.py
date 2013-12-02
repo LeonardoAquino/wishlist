ace5840a1ea4af3c50f63f2d72eb5cd53c869e2e
Aether7 <seba.real.a@gmail.com>
2013-12-02 17:33:18 -0400

    se agregan archivos de dependencias

--- crowdfunding/deseos/models.py
+++ crowdfunding/deseos/models.py
@@ -7,24 +7,40 @@ class Producto(models.Model):
     precio = models.IntegerField(default=0)
     es_recomendado = models.BooleanField(default=False)
 
+    def __unicode(self):
+        return self.nombre + " " + self.url
+
 class Lista(models.Model):
-    titulo = models.CharField(max_length=250)
+    titulo = models.CharField(max_length=250, unique=True)
     descripcion = models.TextField()
     creador = models.ForeignKey(User,related_name="creador")
     contribuyentes = models.ManyToManyField(User, related_name="contribuyentes")
     fecha_creacion = models.DateField(auto_now_add=True)
     fecha_modificacion = models.DateField(auto_now=True,auto_now_add=True)
+    terminado = models.BooleanField(default = False)
+
+    def __unicode__(self):
+        return self.titulo + " " + self.terminado
 
 class Region(models.Model):
     nombre = models.CharField(max_length=140)
+
+    def __unicode(self):
+        return self.nombre
 
 class Comuna(models.Model):
     nombre = models.CharField(max_length=140)
     codigo = models.IntegerField()
     region = models.ForeignKey(Region)
 
+    def __unicode(self):
+        return self.nombre + ", " + self.region.nombre
+
 class TipoCuenta(models.Model):
     nombre = models.CharField(max_length=140)
+
+    def __unicode(self):
+        return self.nombre
 
 class CuentaCorriente(models.Model):
     numero_cuenta = models.IntegerField()
@@ -35,3 +51,7 @@ class DetalleUsuario(models.Model):
     rut = models.CharField(max_length=40,unique=True)
     comuna = models.ForeignKey(Comuna)
     cuenta_corriente = models.ForeignKey(CuentaCorriente)
+
+    def __unicode(self):
+        nombre_usuario = self.usuario.first_name + " " + self.usuario.last_name
+        return nombre_usuario + ", " + self.rut
--- crowdfunding/deseos/views.py
+++ crowdfunding/deseos/views.py
@@ -1,6 +1,18 @@
 # Create your views here.
+from django.views.generic import ListView
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def index(req):
-    return render_to_response("index.html",context_instance=RequestContext(req))
+from .models import Lista
+
+class ProyectosList(ListView):
+    model = Lista
+    template_name = "index.html"
+
+    def get_context_data(self,**kwargs):
+        context = super(ProyectosList, self).get_context_data(**kwargs)
+        context["listas"] = self.model.objects.all()
+
+        return context
+
+index = ProyectosList.as_view()
