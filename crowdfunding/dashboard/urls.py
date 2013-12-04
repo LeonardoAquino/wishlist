2aeac3d37d2736a8392d138d98a8b543726de58d
Norman Glaves <n.glaves@gmail.com>
2013-12-04 20:37:11 -0300

    Se añade Guardar
    se añaden cosas iniciales para guardar nuevo proyecto

--- crowdfunding/dashboard/urls.py
+++ crowdfunding/dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('dashboard.views',
     url(r'^mis_proyectos/', "mis_proyectos", name="mis_proyectos"),
     url(r'^mi_perfil/', "mi_perfil", name="mi_perfil"),
     url(r'^nuevo_proyecto/$',"nuevo_proyecto", name="nuevo_proyecto"),
+    url(r'^guardar_nuevo_proyecto/$',"guardar_nuevo_proyecto", name="guardar_nuevo_proyecto"),
 )
--- crowdfunding/dashboard/views.py
+++ crowdfunding/dashboard/views.py
@@ -17,7 +17,6 @@ class MisProyectosView(ListView):
     def get_context_data(self,**kwargs):
         context = super(MisProyectosView, self).get_context_data(**kwargs)
         mi_usuario = self.request.user
-        print mi_usuario
         context["mis_proyectos"] = self.model.objects.filter(creador_id = mi_usuario.id)
         return context
 
@@ -30,7 +29,11 @@ class NuevoProyectoView(TemplateView):
     template_name = "nuevo_proyecto.html"
 
 
+class GuardarNuevoProyectoView(TemplateView)
+    template_name = "guardarnuevoproyecto.html"    
+
 dashboard = login_required(DashboardView.as_view())
 mis_proyectos = login_required(MisProyectosView.as_view())
 mi_perfil = login_required(MiPerfilView.as_view())
 nuevo_proyecto = login_required(NuevoProyectoView.as_view())
+guardar_nuevo_proyecto = login_required(GuardarNuevoProyectoView.as_view())
