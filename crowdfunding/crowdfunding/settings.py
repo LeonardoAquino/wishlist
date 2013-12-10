519e7c7a7bd568bcd05049e23f78d1025e4ee6df
aether7 <seba.real.a@gmail.com>
2013-12-09 22:27:06 -0300

    data

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -12,16 +12,28 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PARENT_FOLDER, "db.sqlite3"),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',# Set to empty string for default.
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.path.join(PARENT_FOLDER, "db.sqlite3"),
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'PORT': '',# Set to empty string for default.
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE" : "django.db.backends.postgresql_psycopg2",
+            "NAME" : "together",
+            "USER" : "together_user",
+            "PASSWORD" : "together_password",
+            "HOST" : "",
+            "PORT" : ""
+        }
+    }
 
 ALLOWED_HOSTS = []
 
