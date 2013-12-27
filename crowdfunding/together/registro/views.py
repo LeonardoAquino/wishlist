9963dad23a70768a7f4b6d1af99b1fb00d407641
Sebastian Real <seba.real.a@gmail.com>
2013-12-27 00:32:25 -0300

    se corrige registro y ahora no pide comuna obligatoria

--- crowdfunding/together/registro/views.py
+++ crowdfunding/together/registro/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import View, TemplateView
 
-from ..models import Region, Comuna, DetalleUsuario
+from ..models import DetalleUsuario
 from ..common import is_text_valid, is_email_valid
 
 class RegistroView(TemplateView):
@@ -18,15 +18,14 @@ class RegistroView(TemplateView):
 
     def get_context_data(self, **kw):
         data = super(RegistroView, self).get_context_data(**kw)
-        regiones = Region.objects.all()
 
         data = {
-            "regiones" : regiones,
-            "comunas" : [],
             "dias" : [i + 1 for i in range(31)],
             "meses" : [],
             "anios" : [i for i in range(1975, datetime.now().year - 15)]
         }
+
+        data["anios"] = sorted(data["anios"],reverse=True)
 
         for monthnumber in range(1,13):
             data["meses"].append({
@@ -91,8 +90,6 @@ class EnvioView(View):
     def post(self, req):
         self.nombre_usuario = req.POST.get("nombre_usuario")
         self.email = req.POST.get("email")
-        self.region = req.POST.get("region")
-        self.comuna = req.POST.get("comuna")
         self.clave = req.POST.get("password")
         self.sexo = req.POST.get("sexo")
         self.dia = req.POST.get("dia")
@@ -102,8 +99,6 @@ class EnvioView(View):
         valido = True
         valido = valido and is_text_valid(self.nombre_usuario)
         valido = valido and is_email_valid(self.email)
-        #valido = valido and is_text_valid(self.region)
-        #valido = valido and is_text_valid(self.comuna)
         valido = valido and is_text_valid(self.clave)
         valido = valido and is_text_valid(self.sexo)
         valido = valido and is_text_valid(self.dia)
@@ -125,12 +120,10 @@ class EnvioView(View):
         user.save()
 
         fecha_nacimiento = date(int(self.anio), int(self.mes), int(self.dia))
-        comuna = Comuna.objects.get(pk = self.comuna)
 
         detalle = DetalleUsuario()
         detalle.fecha_nacimiento = fecha_nacimiento
         detalle.usuario = user
-        detalle.comuna = comuna
         detalle.sexo = self.sexo
         detalle.save()
 
