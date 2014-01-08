6fc82d0973af9f36838ef8e53b690a8a3792a511
Sebastian Real <seba.real.a@gmail.com>
2014-01-07 23:44:26 -0300

    cambios , para que no pida por obligación fb_id

--- crowdfunding/together/models.py
+++ crowdfunding/together/models.py
@@ -153,7 +153,7 @@ class CuentaBancaria(models.Model):
 
 class DetalleUsuario(models.Model):
     usuario = models.ForeignKey(User)
-    fb_id = models.IntegerField()
+    fb_id = models.IntegerField(null=True,blank=True)
     rut = models.CharField(max_length=40, unique=True, null=True, blank=True)
     comuna = models.ForeignKey(Comuna, null=True, blank=True)
     sexo = models.BooleanField(default=True)
