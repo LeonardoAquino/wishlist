048ad6c213b619ed3f68d93fb62f7b93576a7ac4
Sebastian Real <seba.real.a@gmail.com>
2014-01-12 16:52:10 -0300

    otro commit

--- crowdfunding/together/context_processor.py
+++ crowdfunding/together/context_processor.py
@@ -2,5 +2,5 @@ def is_logged_by_facebook(request):
     esta_logueado_por_facebook = request.session.get("is_logged_by_facebook")
 
     return {
-        "logged_by_facebook" : esta_logueado_por_facebook
+        "logged_by_facebook" : True#esta_logueado_por_facebook
     }
