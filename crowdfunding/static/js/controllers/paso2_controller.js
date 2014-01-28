4324634e36604936ad2dc1631a8eea63eeae5594
Sebastian Real <seba.real.a@gmail.com>
2014-01-28 19:44:12 -0300

    se arreglan las validaciones de números de cuenta

--- crowdfunding/static/js/controllers/paso2_controller.js
+++ crowdfunding/static/js/controllers/paso2_controller.js
@@ -7,20 +7,60 @@ App.Controllers.Paso2 = function(){
     this.$btnContinuar = $("#btn_continuar");
     this.$form = $("#f_paso_2");
 
+    this.mensajeObligatorio = "campo obligatorio";
+    this.mensajeRut = "Rut inválido";
     this.addEvents();
 }
 
 App.Controllers.Paso2.mixin({
     addEvents: function(){
-        console.log("iniciando eventos");
         this.$btnContinuar.on("click", this.validarEnvio.bind(this));
     },
 
     validarEnvio: function(){
         var valido = true;
 
-        if(valido){
-            this.$form.trigger("submit");
+        $(".error_message").removeClass("error_message").empty();
+
+        if(this.isEmpty(this.$titularCuenta)){
+            valido = false;
+            this.$titularCuenta.siblings("span").addClass("error_message").text(this.mensajeObligatorio);
         }
+
+        if(this.isEmpty(this.$numCuenta)){
+            valido = false;
+            this.$numCuenta.siblings("span").addClass("error_message").text(this.mensajeObligatorio);
+        }else if(isNaN(this.$numCuenta.val())){
+            valido = false;
+            this.$numCuenta.siblings("span").addClass("error_message").text("Deben ser sólo números");
+        }
+
+        if(this.isEmpty(this.$rut)){
+            valido = false;
+            this.$rut.siblings("span").addClass("error_message").text(this.mensajeObligatorio);
+        }else if(!$.Rut.validar(this.$rut.val())){
+            valido = false;
+            this.$rut.siblings("span").addClass("error_message").text(this.mensajeRut);
+        }
+
+        if(this.$banco.val() === "0"){
+            valido = false;
+           this.$banco.siblings("span").addClass("error_message").text(this.mensajeObligatorio);
+        }
+
+        if(this.$tipoCuenta.val() === "0"){
+            valido = false;
+            this.$tipoCuenta.siblings("span").addClass("error_message").text(this.mensajeObligatorio);
+        }
+
+        if(!valido){
+            return;
+        }
+
+        this.$form.trigger("submit");
+    },
+
+    isEmpty: function($el){
+        return $el.val().trim() === "";
     }
 });
