e6f652b512cc62a20c4c3e5029edb616e3204a37
Sebastian Real <seba.real.a@gmail.com>
2014-03-21 16:01:51 -0300

    cambios a tipos de proyecto a categoría

--- crowdfunding/static/js/controllers/paso1_controller.js
+++ crowdfunding/static/js/controllers/paso1_controller.js
@@ -1,5 +1,4 @@
 App.Controllers.Paso1 = function(cantidadImpuesto){
-    this.btnOtroProducto = $("#btn_otro_producto");
     this.htmlTipoMoneda = $("#tipo_moneda_0").html();
     this.listaProductos = $("#lista_productos");
     this.cantidadProductos = 1;
@@ -10,61 +9,7 @@ App.Controllers.Paso1 = function(cantidadImpuesto){
 
 App.Controllers.Paso1.mixin({
     addEvents: function(){
-        this.btnOtroProducto.on("click", this.cargarPlantilla.bind(this));
-        $("#lista_productos").on("keyup","input[id^=valor_]", this.calcularMonto(this.cantidadImpuesto));
-        $("#lista_productos").on("click","a[data-accion=eliminar_producto]", this.eliminarProducto());
         $("#btn_continuar").on("click", this.guardarPaso1);
-    },
-
-    cargarPlantilla: function(){
-        var plantilla = templateLoader.render("/templates/nuevo_proyecto/nuevo_producto",{
-            numero : this.cantidadProductos++,
-            monedas : this.htmlTipoMoneda
-        });
-
-        this.listaProductos.append(plantilla);
-    },
-
-    calcularMonto: function(cantidadImpuesto){
-        return function(){
-            var total = 0;
-
-            $("input[id^=valor_]").each(function(){
-                var valor = parseInt($(this).val(), 10);
-
-                if(!type.isNaN(valor)){
-                    total += valor;
-                }
-            });
-
-            $("#total").val(total);
-            $("#con_impuesto").val(total + (total * (cantidadImpuesto/100)));
-        };
-    },
-
-    eliminarProducto: function(){
-        var _this = this;
-
-        return function(){
-            if(!confirm("¿ Esta seguro(a) de eliminar este producto ?")){
-                return;
-            }
-
-            $(this).closest(".producto").remove();
-            _this.recalcularProductos();
-        };
-    },
-
-    recalcularProductos: function(){
-        $(".producto").each(function(index, producto){
-            $(producto).find("*[id]").each(function(i, elem){
-                var id = $(this).attr("id");
-                id = id.replace(/\d+$/, index);
-                $(this).attr("id", id);
-            });
-        });
-
-        this.calcularMonto(this.cantidadImpuesto);
     },
 
     guardarPaso1: function(){
