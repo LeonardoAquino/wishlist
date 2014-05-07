13fee77986cad828d1822335bd5adf327a96756b
aether7 <seba.real.a@gmail.com>
2014-05-07 11:01:26 -0400

    para que el quees diga potato

--- crowdfunding/static/js/controllers/categorias_controller.js
+++ crowdfunding/static/js/controllers/categorias_controller.js
@@ -1,14 +1,12 @@
 App.Controllers.Categoria = function(urlConsulta){
     this.categorias = $("#sel_categoria");
     this.urlConsulta = urlConsulta.replace("/99/", "/");
-    console.log(this.urlConsulta);
     this.init();
 };
 
 App.Controllers.Categoria.prototype = {
     constructor: App.Controllers.Categoria,
     init: function(){
-        console.log("pasando por aqui");
         this.categorias.on("change", this.cambiarCategorias());
     },
 
@@ -19,7 +17,6 @@ App.Controllers.Categoria.prototype = {
             var nuevaUrl = _this.urlConsulta + $(this).val() + "/";
 
             $.get(nuevaUrl, function(data){
-                console.log(data);
                 var html = templateLoader.render("/templates/ajax/proyecto_list", data);
                 $("#proyectos").empty().html(html);
             });
